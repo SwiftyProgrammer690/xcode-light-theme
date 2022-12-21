e22fec0aed87b9106be6a8b3981437213a18b1a7
GitHub Action <action@github.com>
2022-12-21 21:38:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/swiftyprogrammer690/xcodelighttheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/swiftyprogrammer690/xcodelighttheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.swiftyprogrammer690.xcodelighttheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.swiftyprogrammer690.xcodelighttheme.MyBundle
 
 class MyProjectService(project: Project) {
 
